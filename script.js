3fdbadfcf2f029d1ada4b92bb64d3ebd47bdeae6
Sonech <ndivid@live.com>
2018-08-08 11:20:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,29 @@
 // First parameter is the event we listen to. Second parameter is the function to be executed when the event is loaded.
 // The following event is to ensure that the DOMs are loaded in the page, so that we can access it.
 window.addEventListener('load', function() {
+	// Modify 'results' when clicking the search button
+	var results = document.getElementById('results');
+
 	function search() {
-		console.log('Im searching');
+		// Get hobby field
+		var hobbyField = document.getElementById('hobby');
+		// Get hobby value
+		var hobby = hobbyField.value;
+		console.log(hobby);
+
+		// Get gender field
+		var genderField = document.getElementById('gender');
+		// Get gender option index
+		var s = genderField.selectedIndex;
+		// Get gender value from option index
+		var gender = genderField.options[s].value;
+		console.log(gender);
+
+
+		console.log('Im searching!');
+		// Can be string or HTML
+		results.innerHTML = '<div style="color:red;">hello</div>world<br/>new line<div class="person-row">some stuff</div>';
+		console.log(results.innerHTML)
 		}
 
 	// Access search button object
