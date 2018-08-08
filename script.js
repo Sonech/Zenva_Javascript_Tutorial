4f0499f566902ad02579c30a63f8cf577dbc3b91
Sonech <ndivid@live.com>
2018-08-08 10:39:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,12 @@
 // First parameter is the event we listen to. Second parameter is the function to be executed when the event is loaded.
 // The following event is to ensure that the DOMs are loaded in the page, so that we can access it.
 window.addEventListener('load', function() {
-	console.log('the page has been loaded!');
-})
+	function search() {
+		console.log('Im searching');
+		}
+
+	// Access search button object
+	var searchBtn = document.getElementById('searchBtn');
+	// Listen for click event on search button
+	searchBtn.addEventListener('click', search) 
+	});
